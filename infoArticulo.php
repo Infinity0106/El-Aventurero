b4d6d97ea8409c8250215168d106977ff68256d7
Infinity0106 <berny.orozco@hotmail.com>
2016-11-16 10:26:07 -0600

    change sensor

--- infoArticulo.php
+++ infoArticulo.php
@@ -309,7 +309,7 @@ switch ($_GET['type']) {
     <!-- /.container -->
 
 
-    <script type="text/javascript" src="http://maps.google.com/maps/api/js?key=AIzaSyBi1S4mrPa_omAkfUh0IhjeyE2Y496SGdg"></script>
+    <script type="text/javascript" src="http://maps.google.com/maps/api/js?key=AIzaSyBi1S4mrPa_omAkfUh0IhjeyE2Y496SGdg&sensor=false"></script>
     
     <!-- jQuery -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
